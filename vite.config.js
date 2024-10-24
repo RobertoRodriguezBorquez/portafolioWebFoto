794fc36d406a3a1377adaee620d4ea583106c931
Roberto Rodríguez Bqz <105755674+RobertoRodriguezBorquez@users.noreply.github.com>
2024-10-24 16:55:33 -0300

    modificando vite para que pueda pasar en netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['lightgallery/react/Lightgallery.es5.mjs'],
-    },
-  },
+      external: ['lightgallery/react']
+    }
+  }
 });
