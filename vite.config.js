c7a5197625751fa6d862254c65be66ef61f3eccb
Roberto Rodríguez Bqz <105755674+RobertoRodriguezBorquez@users.noreply.github.com>
2024-10-24 17:04:41 -0300

    modificando vite para netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Obtén el directorio actual equivalente a __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'lightgallery/react': path.resolve(__dirname, 'node_modules/lightgallery/react/Lightgallery.es5.mjs')
+    }
+  },
   build: {
     rollupOptions: {
       external: ['lightgallery/react']
