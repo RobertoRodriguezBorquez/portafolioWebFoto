a7d33059c584d1b78f2918b5aa64c50fe72855bf
Roberto Rodríguez Bqz <105755674+RobertoRodriguezBorquez@users.noreply.github.com>
2024-10-24 14:55:39 -0300

    web funcional v1 primer build

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import LightGallery from "lightgallery/react";
+import PropTypes from 'prop-types';
+import LightGallery from "lightgallery/react/Lightgallery.es5.mjs";
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
@@ -9,188 +9,82 @@ import "lightgallery/css/lg-fullscreen.css";
 import "lightgallery/css/lg-share.css";
 import "lightgallery/css/lg-rotate.css";
 
-import lgZoom from "lightgallery/plugins/zoom";
+
+
+// Función para cargar las imágenes según la categoría
+const getImagesByCategory = (category) => {
+  switch (category) {
+    case "estadio":
+      return [
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-1.webp", alt: "Estadio 1" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-2.webp", alt: "Estadio 2" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-3.webp", alt: "Estadio 3" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-4.webp", alt: "Estadio 4" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-5.webp", alt: "Estadio 5" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-6.webp", alt: "Estadio 6" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-8.webp", alt: "Estadio 8" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-9.webp", alt: "Estadio 9" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-10.webp", alt: "Estadio 10" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-11.webp", alt: "Estadio 11" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-12.webp", alt: "Estadio 12" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-13.webp", alt: "Estadio 13" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-14.webp", alt: "Estadio 14" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-15.webp", alt: "Estadio 15" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-16.webp", alt: "Estadio 16" },
+        { src: "/GgaleriaAlbunFoto/estadio/img-estadio-17.webp", alt: "Estadio 17" },
+      ];
+    case "senderismo":
+      return [
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-1.webp", alt: "Senderismo 1" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-2.webp", alt: "Senderismo 2" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-3.webp", alt: "Senderismo 3" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-4.webp", alt: "Senderismo 4" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-5.webp", alt: "Senderismo 5" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-6.webp", alt: "Senderismo 6" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-7.webp", alt: "Senderismo 7" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-8.webp", alt: "Senderismo 8" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-9.webp", alt: "Senderismo 9" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-10.webp", alt: "Senderismo 10" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-11.webp", alt: "Senderismo 11" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-12.webp", alt: "Senderismo 12" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-13.webp", alt: "Senderismo 13" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-14.webp", alt: "Senderismo 14" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-15.webp", alt: "Senderismo 15" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-16.webp", alt: "Senderismo 16" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-17.webp", alt: "Senderismo 17" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-18.webp", alt: "Senderismo 18" },
+        { src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-20.webp", alt: "Senderismo 20" },
+      ];
+    default:
+      console.warn("Categoría inválida. Galería vacía.");
+      return [];
+  }
+};
 
 export function Gallery({ category }) {
-  // Lógica para cargar las imágenes según la categoría
-  let images;
-  if (category === "estadio") {
-    // Cargar imágenes del álbum de estadio
-    images = [
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-1.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-2.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-3.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-4.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-5.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-6.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-8.webp",
-        alt: "",
-      },
-
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-9.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-10.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-11.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-12.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-13.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-14.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-15.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-16.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/estadio/img-estadio-17.webp",
-        alt: "",
-      },
-
-      // Agrega más imágenes aquí según sea necesario
-    ];
-  } else if (category === "senderismo") {
-    // Cargar imágenes del álbum de senderismo
-    images = [
-      // Agrega las imágenes del álbum de senderismo aquí
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-1.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-2.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-3.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-4.webp",
-        alt: "2",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-5.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-6.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-7.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-8.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-9.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-10.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-11.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-12.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-13.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-14.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-15.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-16.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-17.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-18.webp",
-        alt: "",
-      },
-      {
-        src: "/GgaleriaAlbunFoto/senderismo/img-senderismo-20.webp",
-        alt: "",
-      },
-    ];
-  } else {
-    // Manejar caso por defecto o categoría desconocida
-    console.warn("Invalid category prop. Using an empty gallery as default.");
-    images = [];
-  }
+  const images = getImagesByCategory(category);
 
   const onInit = () => {
-    console.log("lightGallery has been initialized");
+    console.log("lightGallery ha sido inicializado");
   };
 
   return (
     <div className="gallery-container">
       <LightGallery
-        plugins={[lgZoom]}
+        plugins={[]}
+        
         mode="lg-fade"
         pager={false}
         thumbnail={true}
         galleryId={"nature"}
         mobileSettings={{
-          controls: false,
-          showCloseIcon: false,
-          download: false,
+          controls: true,
+          showCloseIcon: true,
+          download: true,
           rotate: false,
         }}
-        elementClassNames="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4  gap-2 m-8"
+        elementClassNames="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2 m-8"
+        onInit={onInit} // Se usa el evento onInit para inicializar la galería
       >
         {images.map((image, index) => (
           <a
@@ -203,10 +97,7 @@ export function Gallery({ category }) {
               alt={image.alt}
               src={image.src}
               className="object-cover rounded-lg w-full h-full"
-              style={{
-                display: "block",
-                transition: "transform 0.2s ease-in-out",
-              }}
+              style={{ display: "block", transition: "transform 0.2s ease-in-out" }}
             />
           </a>
         ))}
@@ -214,3 +105,7 @@ export function Gallery({ category }) {
     </div>
   );
 }
+
+Gallery.propTypes = {
+  category: PropTypes.string.isRequired,  // Validación de tipo para 'category'
+};
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Si usas React
 
 export default defineConfig({
-  plugins: [react()], // Añade los plugins que necesites
+  optimizeDeps: {
+    include: ['lightgallery/react']
+  },
   build: {
-    outDir: 'dist', // Salida de la construcción
-  },
-  server: {
-    open: true, // Abrir el navegador automáticamente al iniciar el servidor
-  },
+    rollupOptions: {
+      external: ['lightgallery/react']
+    }
+  }
 });
